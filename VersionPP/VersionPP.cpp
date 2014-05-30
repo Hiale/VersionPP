6e3ac5823d7b47cec6e660049e68f37a1c2ddce8
Hiale <hiale.dev@gmail.com>
2014-05-30 03:47:28 +0200

    Command line

--- VersionPP/VersionPP.cpp
+++ VersionPP/VersionPP.cpp
@@ -5,7 +5,6 @@
 #include "VersionPP.h"
 #include "VersionFile.h"
 #include "TransformerManager.h"
-
 #include <fstream>
 #include <iostream>
 
@@ -13,13 +12,32 @@ std::unique_ptr<TransformerManager> transformerManager;
 
 int main(int argc, char *argv[])
 {
-	std::cout << "Version++, Version 0.1" << std::endl << "Copyright © 2014 Hiale" << std::endl;
+	std::cout << "Version++, Version 0.5" << std::endl << "Copyright © 2014 Hiale" << std::endl;
+
+	if (argc < 2) {
+		std::cout << "Usage: \"" << argv[0] << "\" \"<filename.h>\"" << std::endl;
+		std::cout << "Example: \"" << argv[0] << "\" \"C:\\dev\\proj\\version.h\"" << std::endl;
+		std::cout << "This will patch the header file with the according version information." << std::endl;
+		std::cout << "If the file does not exist a new template will be created." << std::endl;;
+		std::cout << "Please check the Readme file for additional help." << std::endl;
+		return 2;
+	}
+
+	std::string filename(argv[1]);
+	if (!fileExists(filename)) {
+		std::cout << "File \"" << filename << "\" does not exist. Creating template..." << std::endl;
+		//ToDo
+		return 0;
+	}
+	std::cout << "Reading \"" << filename << "\"..." << std::endl;
 
 	transformerManager = std::unique_ptr<TransformerManager>(new TransformerManager);
 
-	VersionFile inputFile("..\\TestFiles\\DotNet.h");
-	if (!inputFile.read())
-		return -1;
+	VersionFile inputFile(argv[1]);
+	if (!inputFile.read()) {
+		std::cout << "Application will exit now." << std::endl;
+		return 1;
+	}
 
 	while (transformerManager->Transform(inputFile.getProductVersion(), inputFile.getCurrentProductVersion())) {
 	}
@@ -31,32 +49,11 @@ int main(int argc, char *argv[])
 		return 1;
 
 	return 0;
+}
 
-	//boost::posix_time::ptime now = boost::posix_time::second_clock::local_time();
-	//unsigned int build = 0;
-	//unsigned int revision = 0;
-	//DotNetVersion::CalculateBuildRevision(now, build, revision);
-
-	//boost::posix_time::ptime dt = DotNetVersion::CalculateBuildTime(5234, 3389);
-
-	//std::cout << dt;
-
-	if (argc < 2) {
-		std::cout << "Usage: " << argv[0] << " <filename> <arguments>" << std::endl;
-		std::cout << "Example: " << argv[0] << " C:\\dev\\proj\\version.h -VERSION 0.5.*.*";
-		std::cout << "Please check the Readme file for additional help.";
-			/*std::cout << "This will open the <filename> update the version information.";
-			std::cout << "If the file does not exist, a new template file will be created.";
-			std::cout << "You can specify the -VERSION command, which will set the version directly.";
-			std::cout << "Product and file version can be set individually using the -PRODUCT_VERSION and - FILE_VERSION commands.";
-			std::cout << "Versions include four parts: Mayor, Minor, Build and Revision. Example: 1.2.3.4";
-			std::cout << "You can use * in the build and revision parts. Example 1.2.*.*";
-			std::cout << "In this case, the .NET assembly version algorithm is used.";*/
-
-
-
-	}
-
-	return 0;
+bool fileExists(const std::string& filename)
+{
+	std::ifstream f(filename.c_str());
+	return f.good();
 }
 
--- VersionPP/VersionPP.h
+++ VersionPP/VersionPP.h
@@ -3,4 +3,6 @@
 #include <memory>
 #include "TransformerManager.h"
 
+bool fileExists(const std::string& filename);
+
 extern std::unique_ptr<TransformerManager> transformerManager;
